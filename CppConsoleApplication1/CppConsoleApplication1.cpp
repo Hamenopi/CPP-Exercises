ea45bd7fba9653cfbb1152d70e71a6a337cf47e2
Justin Hamilton <JHamilton@Pas.com>
2019-03-05 20:53:25 -0600

    Add functions

--- CppConsoleApplication1/CppConsoleApplication1.cpp
+++ CppConsoleApplication1/CppConsoleApplication1.cpp
@@ -3,7 +3,25 @@
 
 using namespace std;
 
+void line(), message(); //Prototypes
+
 int main()
 {
     cout << "Hello World!" << endl; 
+	line();
+	message();
+	line();
+	cout << "At the end" << endl;
+
+	return 0;
+}
+
+void line()
+{
+	cout << "-----" << endl;
+}
+
+void message()
+{
+	cout << "In function message()." << endl;
 }
